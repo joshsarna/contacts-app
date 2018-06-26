6d0f5cdea0f75dee87875134d2142564192a095c
joshsarna <joshsarna@gmail.com>
2018-06-25 18:38:33 -0700

    make routes to display first contact and all contacts

---
+++ app/controllers/api/contacts_controller.rb
@@ -0,0 +1,11 @@
+class Api::ContactsController < ApplicationController
+  def first_contact_action
+    @contact1 = Contact.first
+    render "one_contact_view.json.jbuilder"
+  end
+
+  def all_contacts_action
+    @contacts = [Contact.first,Contact.second,Contact.third]
+    render "all_contacts_view.json.jbuilder"
+  end
+end
---
+++ app/views/api/contacts/all_contacts_view.json.jbuilder
@@ -0,0 +1,3 @@
+json.first_contact @contacts[0]
+json.second_contact @contacts[1]
+json.third_contact @contacts[2]
